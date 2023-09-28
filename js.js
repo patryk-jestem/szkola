6719b214ef3d23d8a29940f26dd70e0a2c7b8f8b
patryk-jestem <146340970+patryk-jestem@users.noreply.github.com>
2023-09-28 13:20:07 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 10;
+
+document.write(a+b);
+document.write(a-b);
+document.write(a*b);
+document.write(a/b);
+
+let w = false;
+let x = 5.5;
+let y = 'co?' 
+let g = undefined;
+document.write('<br> wartość w,x,y,g <br>',w,x,y,g, '<br>')
+
+let p = 6;
+let o = 2;
+document.write(Math.pow(p,o),'<br>');
+
+const s = 0xFF;
+const d = 037;
+const f = 0xACB;
+let h = 12;
+let j = 0.12e-2;
+
+document.write(h,'<br>',s,"<br>",d,"<br>",f,'<br>',j);
+let c=4;
+let l=5;
+let i=c*l
+document.write('<h1>Mnożenie 4x5= ',c*l,'<br>Dzielenie 20/5= ',i/l)
